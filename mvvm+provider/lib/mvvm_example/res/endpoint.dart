0f5f7f2be7950a5bd9044a2b4f11c18ae35a7210
SameerMohsinIdeas <sameer.mohsin@ideas.com.pk>
2024-09-27 15:03:06 +0500

    Removing endpoints.dart

--- mvvm+provider/lib/mvvm_example/res/endpoint.dart
+++
@@ -1,5 +0,0 @@
-class Endpoints {
-  static var baseApiEndPoint = "https://reqres.in/api/";
-  static var loginApiEndPoint = "${baseApiEndPoint}login";
-  static var signUpApiEndPoint = "${baseApiEndPoint}register";
-}
